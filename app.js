8d2a4c66df4740e983bc12ac430ca717cc1f040d
EC2 Default User <ec2-user@ip-172-31-13-23.us-west-1.compute.internal>
2020-11-11 07:45:25 +0000

    visuals set up

---
+++ app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    // Global Variables
+    var limbs = 0;
+    var words = ["GITHUB", "HEROKU", "HYPHEN", "JAVASCRIPT", "OPERATOR", "PYTHON", "STYLE", "TERNARY", "UNICODE", "ZEBRA"]
+    var word;
+    
+    function chooseWord() {
+        let x = Math.floor(Math.random() * 10);
+        word = words[x];
+    }
+    
+    chooseWord();
+    
+    function displayBlankWord() {
+        for(let i = 0; i < word.length; i++) {
+            $("#word").append(`<div class="wordLtr" id="ltr${i}">0</div>`);
+        }
+    }
+    
+    displayBlankWord();
+    
+    function displayLetters() {
+        let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                       "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+                    
+        for(let i = 0; i < letters.length; i++) {
+            $("#letters").append(` <button class="letter" id="${letters[i]}"
+            value="${letters[i]}"> <label for="${letters[i]}">
+            ${letters[i]}</label>`);
+        }
+    }
+    
+    displayLetters();
+    
+    // Event Listeners
+    $(".letter").on("click", function() {
+        if(!word.includes(this.value)) {
+            addLimb();
+        }
+        else {
+            
+        }
+        this.disabled = true;
+    });
+    
+    
+    
+    function addLimb() {
+        limbs += 1;
+        $("#hangman").attr("src", `img/hangman${limbs}.png`);
+    }
+});
