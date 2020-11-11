5cbdaeaf9ac1fb0665af2f958fb10aba8ae0487e
EC2 Default User <ec2-user@ip-172-31-13-23.us-west-1.compute.internal>
2020-11-11 19:17:59 +0000

    Secret word becomes visible with correct guesses

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
 $(document).ready(function() {
     // Global Variables
     var limbs = 0;
-    var words = ["GITHUB", "HEROKU", "HYPHEN", "JAVASCRIPT", "OPERATOR", "PYTHON", "STYLE", "TERNARY", "UNICODE", "ZEBRA"]
+    var words = ["ARACHNID", "GITHUB", "HEROKU", "HYPHEN", "JAVASCRIPT", "OPERATOR", "PYTHON", "STYLE", "TERNARY", 
+                 "UNICODE", "ZEBRA", "HELICOPTER", "LARYNX", "BASS", "TWILIGHT", "STARLIGHT", "AMERICA", "ANDROID",
+                 "APPLE", "OXEN", "ROBOTIC", "HYPNOTIC", "EXOTIC", "HAZARD", "SWAGGER", "CROCODILE"]
     var word;
     
     function chooseWord() {
-        let x = Math.floor(Math.random() * 10);
+        let x = Math.floor(Math.random() * 26);
         word = words[x];
     }
     
-    chooseWord();
-    
-    function displayBlankWord() {
+    function displayWord() {
         for(let i = 0; i < word.length; i++) {
             $("#word").append(`<div class="wordLtr" id="ltr${i}">${word.charAt(i)}</div>`);
         }
     }
     
-    displayBlankWord();
+    function changeWordLtrColor(letter) {
+        for(let i = 0; i < word.length; i++) {
+            if(document.getElementById(`ltr${i}`).textContent == letter) {
+                document.getElementById(`ltr${i}`).style.color = "white";
+            }
+        }
+    }
     
     function displayLetters() {
         let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
                     
         for(let i = 0; i < letters.length; i++) {
-            $("#letters").append(` <button class="letter" id="${letters[i]}"
+            $("#letters").append(` <button class="letter" id="ltr${i}"
             value="${letters[i]}"> <label for="${letters[i]}">
             ${letters[i]}</label>`);
         }
     }
     
+    function gameOver() {
+        $("#hangman").attr("src", `img/hangman9.png`);
+        $("#message").append("<div>Game Over!</div>");
+    }
+    
+    function addLimb() {
+        if(limbs <= 8) {
+            limbs += 1;
+            $("#hangman").attr("src", `img/hangman${limbs}.png`);
+        }
+        else {
+            gameOver();
+        }
+    }
+    
+    chooseWord();
+    displayWord();
     displayLetters();
     
     // Event Listeners
@@ -38,15 +61,8 @@ $(document).ready(function() {
             addLimb();
         }
         else {
-            
+            changeWordLtrColor(this.value);
         }
         this.disabled = true;
     });
-    
-    
-    
-    function addLimb() {
-        limbs += 1;
-        $("#hangman").attr("src", `img/hangman${limbs}.png`);
-    }
 });
